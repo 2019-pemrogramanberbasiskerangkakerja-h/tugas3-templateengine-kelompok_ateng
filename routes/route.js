66058eca4e96a5cdff734ddbfc814ff7c078239a
fuaddary <fuaddary@gmail.com>
2019-04-08 19:57:58 +0700

    pages fixed

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('index', {layout: 'base', template: 'home-template'});
-  // res.render('index');
 });
+
+router.get('/post', function(req, res, next) {
+  res.render('post', {layout: 'base', template: 'home-template'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {layout: 'static', template: 'home-template'});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {layout: 'static', template: 'home-template'});
+});
+
 
 module.exports = router;
